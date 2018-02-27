2cf5b3810359d18fd566e2177c3eed4115afec21
jecyhw <1147352923@qq.com>
2018-02-27 23:11:25 +0800

    ADD: add profile

--- webapi/src/main/java/cn/cnic/trackrecord/web/api/controller/UserController.java
+++ webapi/src/main/java/cn/cnic/trackrecord/web/api/controller/UserController.java
@@ -41,21 +41,34 @@ public class UserController {
 
     @ApiOperation(value = "用户修改密码")
     @RequestMapping(value = "change/password", method = RequestMethod.POST)
-    public HttpRes<?> changePassword(@RequestBody UserChangePassword userChangePassword) {
+    public HttpRes<AuthUser> changePassword(@RequestBody UserChangePassword userChangePassword) {
         User user = userService.get(userDetailsService.getLoginUser().getId());
         if (passwordEncoder.matches(userChangePassword.getOldPassword(), user.getPassword())) {
             user.setPassword(passwordEncoder.encode(userChangePassword.getNewPassword()));
             if (userService.update(user) > 0) {
-                String token = tokenUtil.createTokenForUser(user);
-                user.setPassword(null);
-                AuthUser userVo = new AuthUser(token, user);
-                return HttpRes.success(userVo);
+                return HttpRes.success(getAuthUser(user));
             }
         }
         return HttpRes.fail();
     }
 
     @ApiOperation(value = "用户修改密码")
+    @RequestMapping(value = "change/profile", method = RequestMethod.POST)
+    public HttpRes<?> changeInfo(@RequestBody User user) {
+        user.setId(userDetailsService.getLoginUser().getId());
+        if (userService.update(user) > 0) {
+            return HttpRes.success(getAuthUser(userService.get(user.getId())));
+        }
+        return HttpRes.fail();
+    }
+
+    private AuthUser getAuthUser(User user) {
+        String token = tokenUtil.createTokenForUser(user);
+        user.setPassword(null);
+        return new AuthUser(token, user);
+    }
+
+    @ApiOperation(value = "用户信息修改")
     @RequestMapping(value = "all", method = RequestMethod.GET)
     public HttpRes<List<User>> getAll() {
         return HttpRes.success(userService.getAllByFields(Lists.newArrayList("id", "name")));
