201d7ab8636908a7e1d2ec4de05d572542d57d07
jecyhw <1147352923@qq.com>
2018-03-18 16:35:24 +0800

    MOD: modify track file state

--- common/src/main/java/cn/cnic/trackrecord/common/enumeration/TrackFileState.java
+++ common/src/main/java/cn/cnic/trackrecord/common/enumeration/TrackFileState.java
@@ -2,15 +2,15 @@ package cn.cnic.trackrecord.common.enumeration;
 
 public enum TrackFileState implements ValuedEnum {
     // 定义中间步骤必须以 ... 结尾
-    UPLOAD_SUCCESS("上传成功待验证...", 0),
-    VERIFYING("验证中...", 1),
-    UNZIPPING("解压中...", 2),
-    EXTRACTING_AND_SAVING("提取和保存中...", 3),
-    FINISH("完成", 4),
-    VERIFY_FAIL("验证失败", -1),
-    UNZIP_FAIL("解压失败", -2),
-    EXTRACT_AND_SAVE_FAIL("提取和保存失败", -3),
-    TRY_EXCEED("重试失败", -4);
+    UPLOAD_SUCCESS("上传成功待验证...", 1),
+    VERIFYING("验证中...", 2),
+    UNZIPPING("解压中...", 3),
+    EXTRACTING_AND_SAVING("提取和保存中...", 4),
+    FINISH("完成", 0),
+    VERIFY_FAIL("验证失败", -2),
+    UNZIP_FAIL("解压失败", -3),
+    EXTRACT_AND_SAVE_FAIL("提取和保存失败", -4),
+    TRY_EXCEED("重试失败", -5);
 
     private String name;
     private int value;
