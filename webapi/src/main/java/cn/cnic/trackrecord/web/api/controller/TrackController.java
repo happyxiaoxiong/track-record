e428b2da6e48f2d1eb7517ced2ef3186dbc66628
jecyhw <1147352923@qq.com>
2018-06-01 16:45:17 +0800

    add comment

--- webapi/src/main/java/cn/cnic/trackrecord/web/api/controller/TrackController.java
+++ webapi/src/main/java/cn/cnic/trackrecord/web/api/controller/TrackController.java
@@ -156,8 +156,8 @@ public class TrackController {
             BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
             if (Objects.nonNull(params.getUserId())) {
                 //数字精确搜索
-                Query timeQuery = IntPoint.newExactQuery("userId", params.getUserId());
-                queryBuilder.add(timeQuery, BooleanClause.Occur.MUST);
+                Query userIdQuery = IntPoint.newExactQuery("userId", params.getUserId());
+                queryBuilder.add(userIdQuery, BooleanClause.Occur.MUST);
             }
             if (StringUtils.isNotBlank(params.getKeyword())) {
                 //多关键字搜索
